736f60b087606e79034f29e2e00428c1c8f479d8
Wenderson <wendersonjunior309@gmail.com>
2025-08-02 17:30:53 -0300

    Moving cursor

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,46 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
 use core::panic::PanicInfo;
+
+static mut VGA_BUFFER: *mut u8 = 0xB8000 as *mut u8;
+
+fn outb(port: u16, data: u8) {
+	unsafe {
+		asm!(
+			"out dx, al",
+			in("dx") port,
+			in("al") data
+		);
+	}
+}
+
+fn move_cursor(pos: usize) {
+	outb(0x3D4, 0xE);
+
+	outb(0x3D5, (pos >> 8) as u8);
+
+	outb(0x3D4, 0xF);
+
+	outb(0x3D5, (pos & 0xFF) as u8);
+}
+
+fn print(string: &[u8]) {
+	for (i, &ch) in string.iter().enumerate() {
+		unsafe {
+			*VGA_BUFFER.offset(i as isize * 2) = ch;
+
+			*VGA_BUFFER.offset(i as isize * 2 + 1) = 0x7;
+		}
+
+		move_cursor(i);
+	}
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-	static HELLO_WORLD: &[u8] = b"Hello, world!";
-
-	let vga_buffer = 0xB8000 as *mut u8;
-
-	for (i, &ch) in HELLO_WORLD.iter().enumerate() {
-		unsafe {
-			*vga_buffer.offset(i as isize * 2) = ch;
-
-			*vga_buffer.offset(i as isize * 2 + 1) = 0x7;
-		}
-	}
+	print(b"Hello, world!");
 
 	loop {}
 }
